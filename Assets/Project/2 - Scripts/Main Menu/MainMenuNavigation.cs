038b395278c7bda3382777517dfac242b66e0797
Victor Aubry <generalflood3@gmail.com>
2018-09-02 14:18:32 +0200

    Add Main Menu

---
+++ Assets/Project/2 - Scripts/Main Menu/MainMenuNavigation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuNavigation : MonoBehaviour
+{
+    [SerializeField] private int _mainMenuSceneID;
+    [SerializeField] private int _gameSceneID;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Escape"))
+        {
+            int sceneID = SceneManager.GetActiveScene().buildIndex;
+            if (this._gameSceneID == sceneID)
+                this.LoadMainMenuScene();
+            else if (this._mainMenuSceneID == sceneID)
+                this.QuitGame();
+        }
+    }
+
+    public void LoadMainMenuScene()
+    {
+        SceneManager.LoadScene(this._mainMenuSceneID, LoadSceneMode.Single);
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(this._gameSceneID, LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
